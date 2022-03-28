70a979b4c8f4405ab18ba41113f87a70a9cf67a1
riasvdv <hey@rias.be>
2022-03-28 13:48:49 +0200

    wip

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -7,9 +7,16 @@ zstyle ':completion:*' accept-exact '*(N)'
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path ~/.cache/zsh
 
-HISTFILE=~/.cache/zsh/zhistory
-HISTSIZE=1000
-SAVEHIST=500
+# Larger bash history (allow 32³ entries; default is 500)
+export HISTSIZE=32768
+export HISTFILESIZE=$HISTSIZE
+export HISTCONTROL=ignoredups
+
+# Make some commands not show up in history
+export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help"
+
+# And include the parameter for ZSH
+export HISTORY_IGNORE="(ls|cd|cd -|pwd|exit|date|* --help)"
 
 # Import ssh keys in keychain
 ssh-add -A 2>/dev/null;
